4505ed88139157826eb9fadf3a1aeb253b911fab
Andrew Richardson <drew.b.richardson@gmail.com>
2012-04-09 21:43:13 -0700

    fix div by 0 in crits

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -16,30 +16,37 @@ class Criterion < ActiveRecord::Base
   end
   def get_value 
     agent = Mechanize.new
-
-    puts url
     @raw_value = agent.get(url.gsub('#{stock}', @stock)).search(xpath)[node_number].inner_text().to_f
   end
+
+
   def raw_value_for_calculations
+
+    #hack to prevent div by 0 and neg number strangeness
+    @raw_value = 0.1 if @raw_value <= 0
+
     if big_is_good 
       return @raw_value
     end
     1/@raw_value
   end
+
+  #criterion receives a list of stocks from which to find highest value and calculate a normalized value
   def self.set_normalized_values stocks
 
     i = 0
     stocks[0].scenario.scenarioCriterions.count.times do 
+
+      #find the highest value of passed in stocks
       @@highest_value = 0
       stocks.each do |stock| 
-        logger.debug "criterion = #{stock.scenario.scenarioCriterions[i].criterion.name} | raw = #{stock.scenario.scenarioCriterions[i].criterion.raw_value_for_calculations}"
-
 
         if (stock.scenario.scenarioCriterions[i].criterion.raw_value_for_calculations> @@highest_value)
           @@highest_value = stock.scenario.scenarioCriterions[i].criterion.raw_value_for_calculations
-          logger.debug "highest = #{@@highest_value }"
         end  
       end  
+
+      #create normalized and weighted values for this criterion based on highest value of passed in stocks
       stocks.each do |stock| 
         stock.scenario.scenarioCriterions[i].criterion.normalized_value = stock.scenario.scenarioCriterions[i].criterion.raw_value_for_calculations/@@highest_value 
         stock.scenario.scenarioCriterions[i].weighted_value = stock.scenario.scenarioCriterions[i].criterion.normalized_value * stock.scenario.scenarioCriterions[i].weight
