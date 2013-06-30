f51dd756051c4245e6b2e8d8de86f99fe64cfa28
Andrew Richardson <drew.b.richardson@gmail.com>
2013-06-30 14:52:05 -0700

    clean up submit code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,19 @@
+
+
 $(function() {
 
- $('a.add_child').live("click", function() {
+  //close entries on button clicks
+  $(".chart_button").click(function () {$(this).parent().slideUp(); }); 
+  $(".rate_button").click(function () {$(this).parent().parent().slideUp(); }); 
+
+  //submit form after enter key when in any textarea
+  $("textarea").keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      $("form").submit();
+    }
+  });
+  $('a.add_child').live("click", function() {
     var association = $(this).attr('data-association');
     var template = $(this).prev().html();
     var regexp = new RegExp('new_' + association, 'g');
@@ -9,7 +22,7 @@ $(function() {
     return false;
   });
 
- $('a.remove_child').live('click', function() {
+  $('a.remove_child').live('click', function() {
     var hidden_field = $(this).prev('input[type=hidden]')[0];
     if(hidden_field) {
       hidden_field.value = '1';
